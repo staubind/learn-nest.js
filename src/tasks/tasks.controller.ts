853cf830ed2ba963aba6a5646bfaa2be4dd1d8a9
Dan St. Aubin <staubindj@gmail.com>
2022-02-02 16:27:44 -0600

    added service and an example of how to use a service inside of a contorller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,30 @@
 import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    // in typescript, when defining param names, you can prefix them with an accessor
+    // when doing this, typescript assumes you'll be using it as a property of the TaskController class
+    constructor(private tasksService: TasksService) {}
+
+    // for now we will use local memory to focus on the core, not to get bogged down in db stuff
+    
+
+    // helloWorld() {
+    //     this.tasksService.dosomething();
+    // }
+}
+
+// this works, too, but above has more sugar.
+// @Controller('tasks')
+// export class TasksController {
+//     tasksService: TasksService;
+
+//     constructor(tasksService: TasksService) {
+//         this.tasksService = tasksService;
+//     }
+
+//     helloWorld() {
+//         this.tasksService.dosomething();
+//     }
+// }
