9be4fed5b9e1490e89aef01d90153d447ee1b5e1
Dan St. Aubin <staubindj@gmail.com>
2022-02-06 10:38:21 -0600

    added more scaffolding for the auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersRepository } from './users.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
+  imports: [
+    TypeOrmModule.forFeature([UsersRepository]),
+  ]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
+import { UsersRepository } from './users.repository';
+import { InjectRepository } from '@nestjs/typeorm'
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectRepository(UsersRepository)
+        private usersRepository: UsersRepository,
+    ){}
+}
